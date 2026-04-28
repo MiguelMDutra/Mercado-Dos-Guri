16571eae10bb07e7f1ddbcb2d60426d14b0dda02
LWBeck <l.beck02@edu.pucrs.br>
2026-04-28 15:56:12 -0300

    Add POST route to create a new cart

--- BackEnd/src/routes/cartRoutes.js
+++ BackEnd/src/routes/cartRoutes.js
@@ -4,6 +4,7 @@ import CartController from "../controller/cartController.js";
 const cartRouter = express.Router();
 
 cartRouter.get("/cart", CartController.getCart);
+cartRouter.post("/cart", CartController.createCart);
 cartRouter.post("/cart/item", CartController.addToCart);
 cartRouter.put("/cart/item/:id", CartController.addToCart);
 cartRouter.delete("/cart/item/:id", CartController.removeFromCart);
